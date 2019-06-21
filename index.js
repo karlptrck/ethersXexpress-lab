4bd113db9f6f17f4d1a69320308aba2b3bdc530e
karlptrck <camota.kp@gmail.com>
2019-06-21 14:32:25 -0400

    lab - submission

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const TicketManager = require('./ticket_manager')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const ticketManager = new TicketManager()
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(bodyParser.json());
+
+app.get('/getTicketAmount', (req, res) => {
+    ticketManager.getTicketAmount().then((amount) => {
+        console.log(amount)
+        res.send(amount)
+    }).catch(err => {
+        res.send(err)
+    })
+})
+
+app.get('/getSwopRefNo', (req, res) => {
+    ticketManager.getSwopRefNo().then((swopRefNo) => {
+        console.log(swopRefNo)
+        res.send(swopRefNo)
+    }).catch(err => {
+        res.send(err)
+    })
+})
+
+app.post('/sellTicket', (req, res) => {
+    ticketManager.sell(req.body).then((txHash) => {
+        console.log(txHash)
+        res.send(txHash)
+    }).catch(err => {
+        res.send(err)
+    })
+})
+
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
